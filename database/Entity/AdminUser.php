64c35f5fb04f0ec4abc59ea4432f1213f522d8e5
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-07 20:25:40 +0200

    refactor: split AdminUser name into firstName and lastName

--- database/Entity/AdminUser.php
+++ database/Entity/AdminUser.php
@@ -4,39 +4,55 @@ declare(strict_types=1);
 
 namespace Vendor\NeoPHP\AdminPackage\Database\Entity;
 
-use Neo\Core\Database\ORM\Mapping\Attribute as ORM;
+use Neo\Core\Database\ORM\Mapping\Attribute\Column;
+use Neo\Core\Database\ORM\Mapping\Attribute\Entity;
+use Neo\Core\Database\ORM\Mapping\Attribute\GeneratedValue;
+use Neo\Core\Database\ORM\Mapping\Attribute\Id;
+use Neo\Core\Database\ORM\Mapping\Attribute\JoinColumn;
+use Neo\Core\Database\ORM\Mapping\Attribute\ManyToOne;
+use Neo\Core\Database\ORM\Mapping\Attribute\Table;
 use Vendor\NeoPHP\AdminPackage\Database\Repository\AdminUserRepository;
 
-#[ORM\Entity(repositoryClass: AdminUserRepository::class)]
-#[ORM\Table(name: 'neo_admin_users')]
-class AdminUser
+#[Entity(repositoryClass: AdminUserRepository::class)]
+#[Table(name: 'neo_admin_users')]
+final class AdminUser
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer', unsigned: true)]
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: 'integer', unsigned: true)]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Column(type: 'string', length: 255, unique: true)]
     private string $email;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[Column(type: 'string', length: 255)]
     private string $password;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private string $name;
+    #[Column(type: 'string', name: 'first_name', length: 100)]
+    private string $firstName;
 
-    #[ORM\ManyToOne(targetEntity: AdminRole::class)]
-    #[ORM\JoinColumn(name: 'role_id', referencedColumnName: 'id', nullable: false)]
+    #[Column(type: 'string', name: 'last_name', length: 100)]
+    private string $lastName;
+
+    #[ManyToOne(targetEntity: AdminRole::class)]
+    #[JoinColumn(name: 'role_id', referencedColumnName: 'id', nullable: false)]
     private AdminRole $role;
 
-    #[ORM\Column(type: 'datetime', name: 'created_at')]
+    #[Column(type: 'datetime', name: 'created_at')]
     private \DateTime $createdAt;
 
-    public function __construct(string $email, string $password, string $name, AdminRole $role)
+    public function __construct(
+        string $email,
+        string $password,
+        string $firstName,
+        string $lastName,
+        AdminRole $role,
+    )
     {
         $this->email = $email;
         $this->password = $password;
-        $this->name = $name;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
         $this->role = $role;
         $this->createdAt = new \DateTime();
     }
@@ -56,9 +72,19 @@ class AdminUser
         return $this->password;
     }
 
-    public function getName(): string
+    public function getFirstName(): string
     {
-        return $this->name;
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
     }
 
     public function getRole(): AdminRole
